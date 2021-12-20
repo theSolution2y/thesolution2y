095131ae1b575f51420fcd5175f1975db8787597
Balendu Rawat <balendurawat1@gmail.com>
2021-12-20 23:23:36 +0530

    11- categoies woring creating and reading in admin dashboard

---
+++ resources/views/backend/category/add.blade.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Category</title>
+    <link href="{{asset('backend')}}/css/styles.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+
+    @if ($errors)
+        @foreach ($errors->all() as $error)
+            <p class="text-danger">{{$error}}</p>
+        @endforeach
+    @endif
+
+    @if (Session::has('success'))
+        <p class="text-success">{{session('success')}}</p>
+        
+    @endif
+
+
+    <form method="post" action="{{url('admin/category')}}" enctype="multipart/form-data">
+        @csrf
+        <table class="table table-bordered">
+            <tr>
+                <th>Title</th>
+                <td><input type="text" name="title" class="form-control" /></td>
+            </tr>
+            <tr>
+                <th>Detail</th>
+                <td><input type="text" name="detail" class="form-control" /></td>
+            </tr>
+            <tr>
+                <th>Image</th>
+                <td><input type="file" name="cat_image" /></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" class="btn btn-primary" />
+                </td>
+            </tr>
+        </table>
+      </form>
+      <a class="float-right btn btn-sm btn-dark tn-dark" href="{{url('admin/category')}}">All data</a>
+
+</body>
+</html>
---
+++ resources/views/backend/category/index.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="{{asset('backend')}}/css/styles.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    
+    <title>Document</title>
+</head>
+<body>
+    <div class="card-header">
+        <a href="{{url("admin/category/create")}}" class="btn btn-primary">Add new Category</a>
+    </div>
+    <div class="card-header">
+        <a href="{{url("admin/dashboard")}}" class="btn btn-primary">Dashboard</a>
+    </div>
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-table me-1"></i>
+            DataTable Example
+        </div>
+        <div class="card-body">
+            <table id="datatablesSimple" class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Action</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    @foreach ($data as $cat )
+                        <tr>
+                            <td>{{$cat->id}}</td>
+                            <td>{{$cat->title}}</td>
+                            <td><img src="{{asset('imgs').'/'.$cat->image}}>}}" width="50"/></td>
+                            <td>
+                                <a href="#">Update</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+</body>
+</html>
