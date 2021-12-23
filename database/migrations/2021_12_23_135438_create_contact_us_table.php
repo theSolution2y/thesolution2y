56af775781bedf83ffe802ad2e1da85cd8fe7e6d
Devyush Raturi <devyushraturi@gmail.com>
2021-12-23 20:04:35 +0530

    migrations for contact us has been created

---
+++ database/migrations/2021_12_23_135438_create_contact_us_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactUsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_us', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name',20);
+            $table->string('last_name',20);
+            $table->string('email',50);
+            $table->integer('mobile');
+            $table->string('message',250);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_us');
+    }
+}
