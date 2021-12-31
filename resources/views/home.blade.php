9c426a5d8fffeeded0a874b0acc85c1f5ded8e76
Shivam-Samant <shivamsamant000@gmail.com>
2021-12-31 21:48:49 +0530

    home page and logo animation has been completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,34 +6,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" type="text/css" href="{{asset('frontend')}}/contact.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('frontend')}}/home.css">
 
 </head>
 <body>
-    <section>
-            <div class="container">
-                <div class="form">
-                    <h2>Contact Us</h2>
-                    <form action="">
-                        <div class="inputBx">
-                            <input type="text" placeholder="Full Name">
-                        </div>
-                        <div class="inputBx">
-                            <input type="email" placeholder="Email">
-                        </div>
-                        <div class="inputBx">
-                            <textarea name="message" id="" placeholder="Type Mesage Here..."></textarea>
-                        </div>
-                        <div class="inputBx">
-                            <input type="submit" value="Submit">
-                        </div>
-                        <p class="text">We would get back to you soon</p>
-                        <p class="text">Don't have an account? <a href="#">Click Here</a></p>
-                    </form>
-                </div>
-            </div>
+    <div class="home">
+        <br>
+        <div class="left">
+            <img src="{{asset('imgs')}}/logo.png" alt="logo">
         </div>
-    </section>
+        <div class="right">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error aliquam exercitationem quia ut officia aut, assumenda quos, distinctio consequatur minus sint. Fugit accusamus ut neque amet, eius a doloremque, similique velit vitae quisquam ipsum veritatis repellat illo. Quos sed eligendi quam a corporis adipisci placeat illo repellat. Soluta dolores suscipit ad corrupti Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, vitae asperiores. Ea rerum culpa accusamus excepturi, obcaecati ducimus aspernatur id! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ea quam quasi eos vero, possimus sequi fugiat qui quaerat itaque nostrum libero delectus voluptates odio, in labore, ipsa mollitia commodi! Amet ad, optio porro praesentium deserunt ducimus quaerat laudantium. Quos iste explicabo a porro facilis nulla aperiam odio, nisi voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nesciunt exercitationem fugiat tenetur obcaecati totam quidem aspernatur quis nostrum nobis, soluta aut excepturi provident laudantium veritatis amet libero labore sequi corporis ex quo! Commodi accusantium eaque modi aliquam, suscipit dignissimos voluptas rerum corrupti assumenda nesciunt iusto rem ad totam quam Lorem, ipsum dolor sit amet consectetur adipisicing voluptatibus voluptas dolorem!       
+            </p>
+            <button>Let's Study</button>
+        </div>  
+    </div>
 </body>
 </html>
 @endsection
