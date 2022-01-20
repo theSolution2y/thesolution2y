c38e7f028fb494829c1f7315cf7531c5f37034b1
Devyush Raturi <devyushraturi@gmail.com>
2022-01-20 20:18:39 +0530

    separation of study and read css files

--- resources/views/study.blade.php
+++ resources/views/study.blade.php
@@ -7,7 +7,7 @@
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Study</title>
-     <link rel="stylesheet" href="{{asset('frontend')}}/reading.css">
+     <link rel="stylesheet" href="{{asset('frontend')}}/study.css">
  </head>
  <body style="background:none;">
     <div class="catsec">
