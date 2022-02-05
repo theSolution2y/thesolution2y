ff1e37b8aec83a44558d358731b36df4b37cd016
Balendu Rawat <balendurawat1@gmail.com>
2022-02-05 20:56:25 +0530

    pdf upload from admin

--- database/migrations/2021_12_16_095546_create_posts_table.php
+++ database/migrations/2021_12_16_095546_create_posts_table.php
@@ -18,10 +18,10 @@ class CreatePostsTable extends Migration
             $table->integer('user_id');
             $table->integer('cat_id');
             $table->string('title');
-            $table->string('thumb');
+            
             $table->string('pdf_path');
             $table->string('detail');
-            $table->string('tags');
+            
             $table->timestamps();
         });
     }
