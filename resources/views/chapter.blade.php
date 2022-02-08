8788b9003a76c9e788e42fbc45c2ac160e6e4b9b
Shivam-Samant <shivamsamant000@gmail.com>
2022-02-08 20:21:24 +0530

    reading page has been completed

--- resources/views/chapter.blade.php
+++ resources/views/chapter.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layout.layout')
 @section('content')
 <!DOCTYPE html>
@@ -10,30 +9,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reading</title>
     <link rel="stylesheet" href="{{asset('frontend')}}/reading.css">
+    <style>
+        #pdf-viewer {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100vh;
+            width: 100vw;
+        }
+    </style>
 </head>
 
 <body>
     <main>
-        <!-- left sidebar  -->
-       {{-- @foreach ($posts as $data)
-       <embed src="{{ asset('storage'.'/'.$data->pdf_path)}}" type="application/pdf">
-        
-       @endforeach --}}
-
-       <div id="pdf-viewer">
-        {{-- <embed src="{{ asset('files/theSolution2y.pdf')}}" type="application/pdf" width="100%" height="100%"> --}}
+        <div id="pdf-viewer">
             @foreach ($posts as $data)
-            <embed src="{{ asset('files/'.$data->title)}}" type="application/pdf" width="100%" height="100%">
-
+            <embed src="{{ asset('files/'.$data->name)}}" type="application/pdf" width="100%" height="100%">
             @endforeach
-
-       </div>
+        </div>
     </main>
 </body>
-{{-- <script>
-    
-PDFObject.embed("{{ route('pdf', ['id' => $id]) }}", "#pdf-viewer");
 
-</script> --}}
 </html>
 @endsection
